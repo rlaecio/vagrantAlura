f3ba8789288ea7a3dc9a434f7d2557f160baed8a
Roque Laecio <rlaecio@hotmail.com>
2018-07-16 17:17:32 +0100

    +

--- manifests/web.pp
+++ manifests/web.pp
@@ -7,4 +7,19 @@ package { ["openjdk-7-jre", "tomcat7", "unzip"]:
     require => Exec["apt-update"]
 }
 
+service { "tomcat7":
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    hasrestart => true, 
+    require => Package["tomcat7"]
+}
 
+file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
+    source => "/vagrant/manifests/vraptor-musicjungle.war",
+    owner => tomcat7,
+    group => tomcat7,
+    mode => 0644,
+    require => Package["tomcat7"],
+    notify => Service["tomcat7"]
+}
