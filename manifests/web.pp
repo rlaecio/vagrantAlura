1f14dbc2fd0be025d24ae1c6e72657db6d9195d6
Roque Laecio <rlaecio@hotmail.com>
2018-07-17 17:07:41 +0100

    mysql install

--- manifests/web.pp
+++ manifests/web.pp
@@ -2,7 +2,15 @@ exec { "apt-update":
   command => "/usr/bin/apt-get update"
 }
 
-package { ["openjdk-7-jre", "tomcat7", "unzip"]:
+
+exec { "musicjungle":
+  command => "mysqladmin -uroot create musicjungle",
+	path => "/usr/bin",
+	require => Service["mysql"]
+}
+
+
+package { ["openjdk-7-jre", "tomcat7", "mysql-server"]:
     ensure => installed,
     require => Exec["apt-update"]
 }
@@ -14,6 +22,15 @@ service { "tomcat7":
     hasrestart => true, 
     require => Package["tomcat7"]
 }
+
+service { "mysql":
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    hasrestart => true, 
+    require => Package["mysql-server"]
+}
+
 
 file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
     source => "/vagrant/manifests/vraptor-musicjungle.war",
